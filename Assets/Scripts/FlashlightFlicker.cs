477a7435db87a9f3d8a73f37de137784c43d498f
Matthew Philip <161418639+Mphilip076@users.noreply.github.com>
2026-04-01 12:08:21 -0500

    added flicker script for flashlight

---
+++ Assets/Scripts/FlashlightFlicker.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using System.Collections;
+
+public class FlashlightFlicker : MonoBehaviour
+{
+    public Light flashlight;
+
+    [Header("Timing")]
+    public float minTimeBetweenFlickers = 5f;
+    public float maxTimeBetweenFlickers = 15f;
+
+    [Header("Flicker Settings")]
+    public int minFlickers = 2;
+    public int maxFlickers = 6;
+    public float flickerDuration = 0.05f;
+
+    void Start()
+    {
+        if (flashlight == null)
+            flashlight = GetComponent<Light>();
+
+        StartCoroutine(FlickerRoutine());
+    }
+
+    IEnumerator FlickerRoutine()
+    {
+        while (true)
+        {
+            float waitTime = Random.Range(minTimeBetweenFlickers, maxTimeBetweenFlickers);
+            yield return new WaitForSeconds(waitTime);
+
+            int flickerCount = Random.Range(minFlickers, maxFlickers);
+
+            for (int i = 0; i < flickerCount; i++)
+            {
+                flashlight.enabled = false;
+                yield return new WaitForSeconds(flickerDuration);
+
+                flashlight.enabled = true;
+                yield return new WaitForSeconds(flickerDuration);
+            }
+        }
+    }
+}
