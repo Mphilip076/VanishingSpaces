975b0a5d0beec7b2c07acea5acd2ce35a931917b
bboini2 <bboin2@uic.edu>
2026-04-09 10:44:43 -0500

    Update Battery.cs

--- Assets/Scripts/Battery.cs
+++ Assets/Scripts/Battery.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 public class Battery : PickableItem
 {    
     [Header("Battery Info")]
-    public int maxCharge = 100; // Maximum amount the battery can hold
-    public int minCharge = 0; // Minimum amount the battery can hold
-    private int currentCharge; // Current charge level of the battery
+    public static int maxCharge = 100; // Maximum amount batteries can hold
+    public static int minCharge = 0; // Minimum amount batteries can hold
+    private int currentCharge; // Current charge level of this battery
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        currentCharge = Random.Range(minCharge, maxCharge); // Initialize battery with a random charge level between min and max
+        currentCharge = Random.Range(minCharge, maxCharge); 
+            // Initialize battery with a random charge level between min and max
     }
 
-    public void UseBattery()
+    public void OnUse()
     {
         FlashlightControl.batteryLevel += currentCharge; // Increase the flashlight's battery level by the current charge of this battery
         if (FlashlightControl.batteryLevel > 100)
