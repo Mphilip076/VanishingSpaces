c56ec64ccd62537726c958cd94d297756b3109c3
bboini2 <bboin2@uic.edu>
2026-04-08 19:58:52 -0500

    Added battery to flashlight
    batteries still haven't been added to scenes

--- Assets/Scripts/Battery.cs
+++ Assets/Scripts/Battery.cs
@@ -1,13 +1,27 @@
+using NUnit.Framework.Internal;
 using UnityEngine;
 
-public class Battery : MonoBehaviour
-{
-    
+public class Battery : PickableItem
+{    
+    [Header("Battery Info")]
+    public int maxCharge = 100; // Maximum amount the battery can hold
+    public int minCharge = 0; // Minimum amount the battery can hold
+    private int currentCharge; // Current charge level of the battery
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // Make sure it shows
-        this.gameObject.SetActive(true);
+        currentCharge = Random.Range(minCharge, maxCharge); // Initialize battery with a random charge level between min and max
+    }
+
+    public void UseBattery()
+    {
+        FlashlightControl.batteryLevel += currentCharge; // Increase the flashlight's battery level by the current charge of this battery
+        if (FlashlightControl.batteryLevel > 100)
+        {
+            FlashlightControl.batteryLevel = 100; // Cap the flashlight's battery level at 100
+        }
     }
 
     // Update is called once per frame
--- Assets/Scripts/FlashlightControl.cs
+++ Assets/Scripts/FlashlightControl.cs
@@ -4,6 +4,8 @@ public class FlashlightControl : MonoBehaviour
 {
     public GameObject lightSource;
     private bool isOn = true;
+    public static int batteryLevel = 100; // Battery level from 0 to 100
+    public int drainTime = 2; // Time in seconds for the battery to drain by 1 unit
 
     void Update()
     {
@@ -12,6 +14,22 @@ public class FlashlightControl : MonoBehaviour
         {
             isOn = !isOn;
             lightSource.SetActive(isOn);
+        }
+
+        // If the flashlight is on, decrease battery level over time
+        // Decreases by 1 every second, so it lasts 
+        //      200 seconds or about 3 minutes and 20 seconds at full charge
+        if (isOn && Time.deltaTime > drainTime)
+        {
+            batteryLevel--;
+
+            if(batteryLevel <= 0)
+            {
+                batteryLevel = 0;
+                isOn = false;
+                lightSource.SetActive(false);
+                Debug.Log("Flashlight battery is dead");
+            }
         }
     }
 }
