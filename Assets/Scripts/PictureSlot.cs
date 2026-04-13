12362394dc5ee974cb9e59f4ebc84a347976dcc0
bboini2 <bboin2@uic.edu>
2026-04-13 09:47:45 -0500

    (probably) Fix picture puzzle
    even if it didn't it won't break anything

--- Assets/Scripts/PictureSlot.cs
+++ Assets/Scripts/PictureSlot.cs
@@ -5,55 +5,15 @@ public class PictureSlot : MonoBehaviour
 {
     public bool inUse;
     public AudioSource placeSound;
-    GameObject pickupPromptUI;
-    TextMeshProUGUI promptText;
 
 
     void Start()
     {
-        pickupPromptUI = GameObject.Find("InteractPromptUI");
-        GameObject promptObj = GameObject.Find("InteractText");
-        if (promptObj != null)
-            promptText = promptObj.GetComponent<TextMeshProUGUI>();
+        
     }
 
     void Update()
     {
-        Collider[] hits = Physics.OverlapSphere(transform.position, 3f);
-        bool playerNearby = false;
-
-        foreach (var hit in hits)
-        {
-            if (hit.CompareTag("Player"))
-            {
-                playerNearby = true;
-                break;
-            }
-        }
-
-        if (playerNearby)
-        {
-            ShowPrompt("Press R to place a picture");
-
-            if (Input.GetKeyDown(KeyCode.R))
-            {
-                CancelInvoke("HidePrompt");
-            }
-        }
-        else
-        {
-            HidePrompt();
-        }
-    }
-
-    void ShowPrompt(string message)
-    {
-        if (pickupPromptUI != null) pickupPromptUI.SetActive(true);
-        if (promptText != null) promptText.text = message;
-    }
-
-    void HidePrompt()
-    {
-        if (pickupPromptUI != null) pickupPromptUI.SetActive(false);
+        
     }
 }
