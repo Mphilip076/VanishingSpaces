33db0ed6f096070c0476464d9bdb1e2521732134
bboini2 <bboin2@uic.edu>
2026-04-18 14:57:11 -0500

    Finished picture puzzle
    might move the frames to different scense

--- Assets/Scripts/PicturePuzzle.cs
+++ Assets/Scripts/PicturePuzzle.cs
@@ -3,15 +3,6 @@ using UnityEngine;
 
 public class PicturePuzzle : MonoBehaviour
 {
-    [Header("Picture Puzzle Settings")]
-    public Vector3 pictureALocation;
-    public Vector3 pictureBLocation;
-    public Vector3 pictureCLocation;
-
-    [Header("Picture Slots")]
-    public PictureSlot slot1;
-    public PictureSlot slot2;
-    public PictureSlot slot3;
 
     [Header("Completion Action")]
     public GameObject completionReward;
@@ -19,20 +10,19 @@ public class PicturePuzzle : MonoBehaviour
     public string completionRewardName;
     public AudioSource completeSound;
 
-    private bool isSolved;
-    
+    private static bool isSolved;
+    private static PicturePuzzle instance;
 
     void Start()
     {
+        if(instance == null) instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
         isSolved = false;
-
-        pictureCLocation = slot1.transform.position; // sun
-        pictureALocation = slot2.transform.position; // moon
-        pictureBLocation = slot3.transform.position; // star
-
-        slot1.inUse = false;
-        slot2.inUse = false;
-        slot3.inUse = false;
     }
 
     void Update()
@@ -49,17 +39,24 @@ public class PicturePuzzle : MonoBehaviour
 
     private void CheckPuzzle()
     {
-        if(Picture.A == null || Picture.B == null || Picture.C == null){
-            Debug.LogWarning("[PicturePuzzle] One or more pictures are missing!");
+        // make sure everything exists
+        if( Picture.A == null || Picture.B == null || Picture.C == null ||
+            PictureSlot.s1 == null || PictureSlot.s2 == null || PictureSlot.s3 == null)
+        {
             return;
         }
 
-        Debug.Log("[PicturePuzzle] Checking puzzle, A at " + Picture.A.transform.position + ", B at " + Picture.B.transform.position + ", C at " + Picture.C.transform.position);
-        Debug.Log("[PicturePuzzle] Target positions: A at " + pictureALocation + ", B at " + pictureBLocation + ", C at " + pictureCLocation);
+        // Slot 1 = moon
+        // Slot 2 = star
+        // Slot 3 = sun
 
-        if(Picture.A.transform.position == pictureALocation &&
-           Picture.B.transform.position == pictureBLocation &&
-           Picture.C.transform.position == pictureCLocation)
+        // Pic A = moon
+        // Pic B = star 
+        // Pic C = sun
+
+        if( PictureSlot.s1.placed.gameObject == Picture.A.gameObject &&
+            PictureSlot.s2.placed.gameObject == Picture.B.gameObject &&
+            PictureSlot.s3.placed.gameObject == Picture.C.gameObject )
         {
             OnSolve();
         }
@@ -69,9 +66,20 @@ public class PicturePuzzle : MonoBehaviour
     {
         isSolved = true;
         Debug.Log("[PicturePuzzle] Puzzle Solved!");
+
+        // Make sure these items cannot be moved!
         Picture.A.canPickUp = false;
+        Picture.A.canInteract = false;
         Picture.B.canPickUp = false;
+        Picture.B.canInteract = false;
         Picture.C.canPickUp = false;
+        Picture.C.canInteract = false;
+        PictureSlot.s1.canInteract = false;
+        PictureSlot.s2.canInteract = false;
+        PictureSlot.s3.canInteract = false;
+        
+        // Give the player the reward
         InventoryManager.Instance.AddItem(completionReward, completionRewardSprite, completionRewardName);
+        completeSound.Play();
     }
 }
--- Assets/Scripts/PictureSlot.cs
+++ Assets/Scripts/PictureSlot.cs
@@ -1,19 +1,130 @@
 using UnityEngine;
 using TMPro;
+using Unity.VisualScripting;
+using System.Data;
 
-public class PictureSlot : MonoBehaviour
+public class PictureSlot : InteractableItem
 {
-    public bool inUse;
+    public PickableItem placed;
     public AudioSource placeSound;
 
+    public static PictureSlot s1;
+    public static PictureSlot s2;
+    public static PictureSlot s3;
 
     void Start()
-    {
-        
+    {    // Prevent duplicates
+        if(this.name == "Slot1" && s1 == null)
+        {
+            s1 = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if(this.name == "Slot2" && s2 == null)
+        {
+            s2 = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if(this.name == "Slot3" && s3 == null)
+        {
+            s3 = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Debug.Log("[PictureSlot] Duplicate picture found, destroying");
+            Destroy(gameObject);
+        }
+
+        interactKey = KeyCode.R;
+        interactRange = 4f;
+        interactMessage = "Press R to place an item";
+        placed = null;
     }
 
-    void Update()
+    public override void OnInteract()
     {
-        
+        if(placed == null) Place();
+        else PickUp();
     }
+
+
+    void PickUp()
+    {
+        if(placed == null) return;
+        
+        // Try to add the item to the inventory
+        bool added = InventoryManager.Instance.AddItem(placed.gameObject, placed.itemIcon, placed.itemName);
+
+        // Failed
+        if(!added) return;
+
+        // Move the item
+        placed.OnPickup(GameObject.Find("HoldPosition").transform);
+
+        // Undo the changes on placing:
+        placed.canPickUp = true;
+        placed.canInteract = true;
+        interactKey = KeyCode.R;
+        interactMessage = "Press R to place an item";
+        
+        // Finally, reset placed:
+        placed = null;
+    }
+
+    void Place()
+    {
+        InventoryManager i = InventoryManager.Instance;
+        GameObject toPlace = i.GetSelectedItem();
+
+        if(placed != null)
+        {
+            Debug.LogWarning("[PictureSlot] Player is trying to place an item but placed is not null");
+            return;
+        }
+
+        // Is the player holding something?
+        if(toPlace == null){ 
+            Debug.Log("[PictureSlot] Currently held item null");
+            return;
+        }
+
+        // Can that item be placed?
+        PickableItem item = toPlace.GetComponentInParent<PickableItem>();
+        if(item == null){ 
+            Debug.Log("[PictureSlot] Could not retrieve PickableItem component");
+            return;
+        }
+
+        // Cant that item be dropped?
+        if(!item.canDrop || item.isFlashlight){
+            Debug.Log("[PictureSlot] Player is trying to place in item that cannot leave the inventory");
+            return;
+        }
+
+        // "drop" the item
+        item.OnDrop();
+
+        // Set the position and rotation to match this
+        item.transform.SetPositionAndRotation(this.transform.position, this.transform.rotation);
+
+        // Remove the item from the inventory        
+        InventoryManager.Instance.RemoveItem(toPlace);
+
+        // Make sure we know it's here
+        placed = item;
+
+        // You can't place an item anymore
+        // Make it look like the player is interacting with the item
+        // even though they're interacting with this:
+        interactKey = KeyCode.E;
+        interactMessage = "Press E to pick up";
+        // The keycode being different prevents place->immediate pickup
+
+        // Void the item's message:
+        placed.canPickUp = false;
+        placed.canInteract = false;
+
+        Debug.Log("[PictureSlot] Placing " + toPlace.name);
+    }
+
 }
