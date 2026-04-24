ed539a04c9786d6663e6230f7f6837fc6a61a53d
bboini2 <bboin2@uic.edu>
2026-04-23 21:13:13 -0500

    Bathroom puzzle WIP

---
+++ Assets/Scripts/BathroomPuzzle.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class BathroomPuzzle : MonoBehaviour
+{
+
+    [Header("Puzzle Settings")]
+    public int correctToiletPosition;
+
+    
+
+    private static BathroomPuzzle instance;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if(instance == null) instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckPuzzleCompletion();
+    }
+
+    void CheckPuzzleCompletion()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/FallenVase.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class FallenVase : InteractableItem
+{
+    public static bool isUpright;
+    private static FallenVase instance;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if(instance == null) instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        interactMessage = "Press E to set upright";
+        isUpright = false;
+    }
+
+    public override void OnInteract()
+    {
+        gameObject.transform.position = new Vector3(-10.850459098815918f, 2.5157351046800616e-7f, -19.1200008392334f);
+        gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+
+        isUpright = true;
+        canInteract = false;
+    }
+}
