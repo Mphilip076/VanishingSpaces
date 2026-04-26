c602217423343747ba5c124f69079707de2b3471
bboini2 <bboin2@uic.edu>
2026-04-25 22:19:27 -0500

    Actually fixed lighter this time
    For some reason the singleton wasn't working before? Now there is only one lighter.

--- Assets/Scripts/lighter.cs
+++ Assets/Scripts/lighter.cs
@@ -3,23 +3,19 @@ using UnityEngine.InputSystem;
 
 public class Lighter : PickableItem
 {
-    private Lighter instance = null;
+    private static bool exists = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public override void Start()
     {
-        if(instance == null) instance = this;
-        else
+        base.Start();
+
+        if(exists)
         {
             Destroy(gameObject);
             return;
         }
 
         DontDestroyOnLoad(gameObject);
-
-        interactKey = KeyCode.E;
-        interactMessage = "Press E to pick up";
-        interactRange = 3f;
-        canInteract = true;
-        canPickUp = true;
+        exists = true;
     }
 }
